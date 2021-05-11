bb72612b5a1e588351a45b9ae4d1e528435c97d1
Eric M.M.C <82389602+emmc316@users.noreply.github.com>
2021-05-11 16:06:30 -0500

    Add files via upload
    First version upleaded

---
+++ DinosaurBattleConsole/Attack.h
@@ -0,0 +1,57 @@
+/*
+Class Attack to define the attacks how the attack works and how much damage will do.
+*/
+
+#ifndef ATAQUE_H_INCLUDED
+#define ATAQUE_H_INCLUDED
+#include <iostream>
+
+class Attack{
+
+private:
+int id;
+std::string name;
+int damage;
+
+public:
+
+    Attack(){
+    };
+
+    Attack(int i,std::string n,int e):
+    id(i),name(n),damage(e){
+    };
+
+    int getId(){
+    return this->id;
+    }
+
+    std::string setId(int i){
+    this->id = i;
+    }
+
+    void setName(std::string n){
+    this->name = n;
+    }
+
+    std::string getName(){
+    return this->name;
+    }
+
+    void setDamage(int e){
+    this->damage = e;
+    }
+
+    int getDamage(){
+    return this->damage;
+    }
+
+    void debug(){
+    std::cout<< "Movement ->"<<" id: "<<this->getId()<<" Name : "<<this->getName()<<" Damage: "<<this->getDamage()<<"\n";
+
+
+    }
+
+};
+
+#endif // ATAQUE_H_INCLUDED
---
+++ DinosaurBattleConsole/main.cpp
@@ -0,0 +1,12 @@
+#include "game.h"
+#include "Dinosaur.h"
+#include "Attack.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+//seed for rand()
+srand(time(NULL));
+start();//function that call all method in game.h
+return 0;
+}
